2fc03bcb1aea562199378ba1edf910e4f5a22d48
codic12 <codic@linuxmail.org>
2021-10-22 15:47:39 -0700

    picom; no need to use experimental backends by default

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 feh --bg-scale /usr/share/endeavouros/backgrounds/endeavouros-wallpaper.png
-picom --experimental-backends --vsync & disown # should prevent screen tearing on most setups
+picom & disown # --experimental-backends --vsync should prevent screen tearing on most setups if needed
