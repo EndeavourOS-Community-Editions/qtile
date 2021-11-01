8527a3d55bae073e76fb36cc1b952ad9c476ffa4
codic12 <48339289+codic12@users.noreply.github.com>
2021-11-01 15:27:58 -0700

    EOS-Welcome in autostart.sh

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -4,3 +4,6 @@ picom & disown # --experimental-backends --vsync should prevent screen tearing o
 
 # Low battery notifier
 ~/.config/qtile/scripts/low_bat_notifier.sh & disown
+
+# Start welcome
+eos-welcome
