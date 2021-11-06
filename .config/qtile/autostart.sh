b8fa6eaceff3a3e124b6ee2a3773fb02ba4e22a4
emanuelep57 <emanuelep57@gmail.com>
2021-11-06 18:11:28 +0100

    Update check_battery and setup file

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -3,7 +3,7 @@ feh --bg-scale /usr/share/endeavouros/backgrounds/endeavouros-wallpaper.png
 picom & disown # --experimental-backends --vsync should prevent screen tearing on most setups if needed
 
 # Low battery notifier
-~/.config/qtile/scripts/low_bat_notifier.sh & disown
+~/.config/qtile/scripts/check_battery.sh & disown
 
 # Start welcome
 eos-welcome & disown
