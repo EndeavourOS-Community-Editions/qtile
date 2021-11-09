656164782f56885826cba8a398c2c3c5da2f830e
emanuelep57 <emanuelep57@gmail.com>
2021-11-09 02:24:20 +0100

    Modified widget, added control for desktops

--- setup.sh
+++ setup.sh
@@ -1,18 +1,6 @@
 # NEEDS BETTER ERROR HANDLING
 # AND BACKUP FILES
 
-
-check_chassis(){
-	line="widget.Battery(battery=1, discharge_char='    ', low_percentage=0.25, charge_char='   ', foreground='5bc236', format='{char} {percent:2.0%}', update_interval=30),"
-	commented_line="#"
-	commented_line+=$line
-	chassis_type=$(cat /sys/class/dmi/id/chassis_type)
-	#chassis numbers go from 1 to 11, from 8 to 11 there are all devices with a battery
-	if [ "$chassis_type" -lt 8 ]; then
-  		sed -i "s/$line/$commented_line/" ~/.config/qtile/config.py
-	fi
-}
-
 echo "Deletes some existing config. Are you sure you want to do this? (yes/no)"
 read x
 if [ "$x" = "yes" ]
@@ -27,7 +15,6 @@ then
   cp -r .config/gtk-3.0 ~/.config/
   rm -rf ~/.config/qtile/
   cp -r .config/qtile ~/.config/
-  check_chassis
   rm -rf ~/.config/rofi/
   cp -r .config/rofi ~/.config/
   rm -rf ~/.config/dunst
