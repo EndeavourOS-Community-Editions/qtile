0b9aab2ba3e7db2cb8d0545d57435e6d7ed3093e
emanuelep57 <80958216+emanuelep57@users.noreply.github.com>
2021-11-18 11:32:28 +0100

    Add files via upload

---
+++ .config/qtile/scripts/logout.py
@@ -0,0 +1,4 @@
+#! /usr/bin/env python3
+from libqtile.command.client import InteractiveCommandClient
+c = InteractiveCommandClient()
+c.shutdown()
---
+++ .config/qtile/scripts/powermenu.sh
@@ -0,0 +1,70 @@
+#!/bin/sh
+
+theme="eos-powermenu"
+dir="$HOME/.config/rofi"
+
+uptime=$(uptime -p | sed -e 's/up //g')
+rofi_command="rofi -theme $dir/$theme"
+
+# Options
+shutdown=""
+reboot=""
+lock=""
+suspend=""
+logout=""
+
+# Confirmation
+confirm_exit() {
+	rofi -dmenu\
+		-i\
+		-no-fixed-num-lines\
+		-p "Are You Sure? : "\
+		-theme $dir/confirm.rasi
+}
+
+
+# Variable passed to rofi
+options="$shutdown\n$reboot\n$lock\n$suspend\n$logout"
+
+chosen="$(echo -e "$options" | $rofi_command -p "Uptime: $uptime" -dmenu -selected-row 2)"
+case $chosen in
+    $shutdown)
+		ans=$(confirm_exit &)
+		if [[ $ans == "yes" || $ans == "YES" || $ans == "y" || $ans == "Y" ]]; then
+			systemctl poweroff
+		elif [[ $ans == "no" || $ans == "NO" || $ans == "n" || $ans == "N" ]]; then
+			exit 0
+        else
+			msg
+        fi
+        ;;
+    $reboot)
+		ans=$(confirm_exit &)
+		if [[ $ans == "yes" || $ans == "YES" || $ans == "y" || $ans == "Y" ]]; then
+			systemctl reboot
+		elif [[ $ans == "no" || $ans == "NO" || $ans == "n" || $ans == "N" ]]; then
+			exit 0
+        fi
+        ;;
+    $lock)
+		if [[ -f /usr/bin/betterlockscreen ]]; then
+			betterlockscreen -l
+		fi
+        ;;
+    $suspend)
+		ans=$(confirm_exit &)
+		if [[ $ans == "yes" || $ans == "YES" || $ans == "y" || $ans == "Y" ]]; then
+			systemctl suspend
+		elif [[ $ans == "no" || $ans == "NO" || $ans == "n" || $ans == "N" ]]; then
+			exit 0
+        fi
+        ;;
+    $logout)
+		ans=$(confirm_exit &)
+		if [[ $ans == "yes" || $ans == "YES" || $ans == "y" || $ans == "Y" ]]; then
+		    ~/.config/qtile/scripts/logout.py
+		elif [[ $ans == "no" || $ans == "NO" || $ans == "n" || $ans == "N" ]]; then
+			exit 0
+        fi
+        ;;
+esac
