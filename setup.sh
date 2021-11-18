e18dd6b1c400cfd5123a1f20a5db818bf76ecfed
emanuelep57 <80958216+emanuelep57@users.noreply.github.com>
2021-11-18 13:37:35 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,7 +6,49 @@ read x
 if [ "$x" = "yes" ]
 then
   echo "Presuming that all dependencies already installed..."
-
+  
+  if  [[ -d ~/.config ]]; then
+      echo "Directory .config found"
+  else
+      cp -r .config ~/
+  fi
+  
+  if [[ -d ~/.local ]]; then
+      echo "~/.local found\n"
+      if [[ -d ~/.local/share ]]; then
+          echo "~/.local/share found\n"
+          
+          if [[ -d ~/.local/share/endeavouros ]]; then
+              cp -r .local/share/endeavouros/* ~/.local/share/endeavouros
+              echo "copying wallpapers to .local/share/endeavouros\n"
+          else
+              mkdir .local/share/endeavouros
+              cp -r .local/share/endeavouros/* ~/.local/share/endeavouros
+              echo "copying wallpapers to .local/share/endeavouros\n"
+          fi
+          
+          if [[ -d ~/.local/share/fonts ]]; then
+              cp -r .local/share/fonts/* ~/.local/share/fonts
+              echo "copying fonts to .local/share/fonts\n"
+          else
+              mkdir .local/share/fonts
+              cp -r .local/share/fonts/* ~/.local/share/fonts
+              echo "copying fonts to .local/share/fonts\n"
+          fi
+      else
+          mkdir ~/.local/share
+          cp -r .local/share/* ~/.local/share
+          echo "Directory ~/.local/share created.\n
+          you can find wallpapers at : ~/.local/share/endeavouros\n
+          you can find fonts at : ~/.local/share/fonts"
+      fi
+  else
+      cp -r .local/* ~/.local/
+      echo "Directory ~/.local/share created.\n
+      you can find wallpapers at : ~/.local/share/endeavouros\n
+      you can find fonts at : ~/.local/share/fonts"
+  fi
+  
   rm -f ~/.Xresources ~/.gtkrc-2.0 ~/.config/picom.conf
   cp .Xresources ~/
   cp .gtkrc-2.0 ~/
