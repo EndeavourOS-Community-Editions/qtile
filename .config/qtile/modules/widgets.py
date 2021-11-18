41359051e89738d761d3ddb7d355c7e20eec70c4
emanuelep57 <80958216+emanuelep57@users.noreply.github.com>
2021-11-18 11:27:48 +0100

    Update widgets.py

--- .config/qtile/modules/widgets.py
+++ .config/qtile/modules/widgets.py
@@ -63,23 +63,23 @@ class MyVolume(widget.Volume):
         widget.Volume._configure(self, qtile, bar)
         self.volume = self.get_volume()
         if self.volume <= 0:
-            self.text = '婢'
+            self.text = '婢 '
         elif self.volume <= 33:
-            self.text = ''
+            self.text = ' '
         elif self.volume < 66:
-            self.text = '墳'
+            self.text = '墳 '
         else:
-            self.text = ''
+            self.text = '  '
 
     def _update_drawer(self, wob=False):
         if self.volume <= 0:
-            self.text = '婢'
+            self.text = '婢 '
         elif self.volume <= 33:
-            self.text = ''
+            self.text = ' '
         elif self.volume < 66:
-            self.text = '墳'
+            self.text = '墳 '
         else:
-            self.text = ''
+            self.text = '  '
         self.draw()
 
         if wob:
