82744b5374b5d2c4cfaf2d589f960958378bd3d6
emanuelep57 <80958216+emanuelep57@users.noreply.github.com>
2021-11-18 11:26:58 +0100

    Update widgets.py

--- .config/qtile/modules/widgets.py
+++ .config/qtile/modules/widgets.py
@@ -2,23 +2,26 @@ from libqtile import widget
 from libqtile.widget.battery import Battery, BatteryState
 from libqtile import qtile
 
+# These are the colors if you're using a laptop.
 colors = [
-	      ["#282c34", "#282c34"], # panel background
-          ["#3d3f4b", "#434758"], # background for current screen tab
-          ["#ffffff", "#ffffff"], # font color for group names
-          ["#ff5555", "#ff5555"], # border line color for current tab
-          ["#74438f", "#74438f"], # border line color for 'other tabs' and color for 'odd widgets'
-          ["#4f76c7", "#4f76c7"], # color for the 'even widgets'
-          ["#e1acff", "#e1acff"], # window name
-          ["#ecbbfb", "#ecbbfb"]  # backbround for inactive screens
-] 
+    ["#27173b"], #0 color of the bar
+    ["#a05ae6"], #1 color of layout widget
+    ["#FF7F7F"], #2 color of volume widget
+    ["#7F3FBF"], #3 color of battery widget
+    ["#7f7fff"], # 4 color of wifi widget 
+    ["#d87aeb"], # 5 color of clock widget
+    ["#FFBEBE"], # 6 color or powermenu widget
+    ["#ff0000"], # 7color of low foreground battery
+]
 
-
-widget_defaults = dict(
-    font='sans',
-    fontsize=12,
-    padding=3,
-)
+#These are the colors if you're using a desktop. The color of the bar is common.
+colors2 = [
+    ["#a286db"], # 0 color of the layout widget
+    ["#f28a8a"], # 1 color of the volume widget
+    ["#8166b0"], #2 color of the connection widget
+    ["#9d9de3"], #3 color of the clock widget
+    ["#f1b1fc"], #4 color of the powermenu wwidget
+]
 
 class MyBattery(Battery):
     def build_string(self, status):
@@ -28,9 +31,11 @@ class MyBattery(Battery):
             else:
                 self.layout.colour = self.foreground
         if status.state == BatteryState.DISCHARGING:
-            if status.percent > 0.75:
+            if status.percent >= 0.75:
+                char = ''
+            if status.percent >= 0.60:
                 char = ''
-            elif status.percent > 0.45:
+            elif status.percent >= 0.45:
                 char = ''
             else:
                 char = ''
@@ -45,11 +50,11 @@ class MyBattery(Battery):
 
 battery = MyBattery(
     format='{char}  {percent:2.0%}',
-    low_foreground=colors[1],
+    low_foreground=colors[7],
     show_short_text=False,
     low_percentage=0.25,
-    foreground=colors[6],
-    notify_below=12,
+    font="RobotoCondensed",
+    background=colors[3],
 )
 
 
@@ -58,22 +63,21 @@ class MyVolume(widget.Volume):
         widget.Volume._configure(self, qtile, bar)
         self.volume = self.get_volume()
         if self.volume <= 0:
-            self.text = ''
-        elif self.volume <= 15:
-            self.text = ''
-        elif self.volume < 50:
+            self.text = '婢'
+        elif self.volume <= 33:
             self.text = ''
+        elif self.volume < 66:
+            self.text = '墳'
         else:
             self.text = ''
-        # drawing here crashes Wayland
 
     def _update_drawer(self, wob=False):
         if self.volume <= 0:
-            self.text = ''
-        elif self.volume <= 15:
-            self.text = ''
-        elif self.volume < 50:
+            self.text = '婢'
+        elif self.volume <= 33:
             self.text = ''
+        elif self.volume < 66:
+            self.text = '墳'
         else:
             self.text = ''
         self.draw()
@@ -82,9 +86,4 @@ class MyVolume(widget.Volume):
             with open(self.wob, 'a') as f:
                 f.write(str(self.volume) + "\n")
 
-volume = MyVolume(
-    fontsize=18,
-    font='Font Awesome 5 Free',
-    foreground=colors[4],
-    mouse_callbacks = {'Button1': lambda: qtile.cmd_spawn("pavucontrol")}
-)
+
