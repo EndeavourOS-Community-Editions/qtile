5dde46bd905251b49bbd9a34243241bbe069fd1c
emanuelep57 <80958216+emanuelep57@users.noreply.github.com>
2021-11-18 11:31:50 +0100

    Update autostart.sh

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -1,9 +1,22 @@
 #!/bin/sh
-feh --bg-scale /usr/share/endeavouros/backgrounds/endeavouros-wallpaper.png
-picom & disown # --experimental-backends --vsync should prevent screen tearing on most setups if needed
 
-# Low battery notifier
-~/.config/qtile/scripts/check_battery.sh & disown
+#start script to autolock screen and autosuspend when the computer is inactive.
+~/.config/qtile/scripts/locker &
+
+#dunst notifications
+/usr/bin/dunst &
+
+#setting the wallpaper
+feh --bg-scale ~/.local/share/endeavouros/eos-wallpaper.png
+
+#check if battery is present, if so runs the script to send notifications when battery is low.
+# couldn't make it work the notify built-in in qtile
+if [ "$(ls -A /sys/class/power_supply)" ]; then
+	~/.config/qtile/scripts/check_battery.sh &
+fi
+
+#start compositor
+picom --experimental-backends &
 
 # Start welcome
 eos-welcome & disown
